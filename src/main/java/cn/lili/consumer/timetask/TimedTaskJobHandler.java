5329a6a0f474ef797f46d765e026fdc4b65d90be
jiangood <410518072@qq.com>
2026-02-07 12:33:31 +0800

    去掉xxljob，使用springboot的定时任务

--- src/main/java/cn/lili/consumer/timetask/TimedTaskJobHandler.java
+++ src/main/java/cn/lili/consumer/timetask/TimedTaskJobHandler.java
@@ -3,10 +3,9 @@ package cn.lili.consumer.timetask;
 import cn.lili.consumer.timetask.handler.EveryDayExecute;
 import cn.lili.consumer.timetask.handler.EveryHourExecute;
 import cn.lili.consumer.timetask.handler.EveryMinuteExecute;
-import com.xxl.job.core.biz.model.ReturnT;
-import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -38,11 +37,11 @@ public class TimedTaskJobHandler {
      *
      * @throws Exception
      */
-    @XxlJob("everyMinuteExecute")
-    public ReturnT<String> everyMinuteExecute(String param)  {
+    @Scheduled(cron = "0 * * * * ?")
+    public void everyMinuteExecute()  {
         log.info("每分钟任务执行");
         if (everyMinuteExecutes == null || everyMinuteExecutes.size() == 0) {
-            return ReturnT.SUCCESS;
+            return;
         }
 
         for (int i = 0; i < everyMinuteExecutes.size(); i++) {
@@ -52,7 +51,6 @@ public class TimedTaskJobHandler {
                 log.error("每分钟任务异常", e);
             }
         }
-        return ReturnT.SUCCESS;
     }
 
     /**
@@ -60,11 +58,11 @@ public class TimedTaskJobHandler {
      *
      * @throws Exception
      */
-    @XxlJob("everyHourExecuteJobHandler")
-    public ReturnT<String> everyHourExecuteJobHandler(String param) {
+    @Scheduled(cron = "0 0 * * * ?")
+    public void everyHourExecuteJobHandler() {
         log.info("每小时任务执行");
         if (everyHourExecutes == null || everyHourExecutes.size() == 0) {
-            return ReturnT.SUCCESS;
+            return;
         }
 
         for (int i = 0; i < everyHourExecutes.size(); i++) {
@@ -74,7 +72,6 @@ public class TimedTaskJobHandler {
                 log.error("每小时任务异常", e);
             }
         }
-        return ReturnT.SUCCESS;
     }
 
     /**
@@ -82,12 +79,12 @@ public class TimedTaskJobHandler {
      *
      * @throws Exception
      */
-    @XxlJob("everyDayExecuteJobHandler")
-    public ReturnT<String> everyDayExecuteJobHandler(String param) {
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void everyDayExecuteJobHandler() {
 
         log.info("每日任务执行");
         if (everyDayExecutes == null || everyDayExecutes.size() == 0) {
-            return ReturnT.SUCCESS;
+            return;
         }
 
         for (int i = 0; i < everyDayExecutes.size(); i++) {
@@ -97,7 +94,6 @@ public class TimedTaskJobHandler {
                 log.error("每日任务异常", e);
             }
         }
-        return ReturnT.SUCCESS;
     }
 
 
